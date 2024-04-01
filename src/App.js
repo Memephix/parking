d58b65117071f15493c9063eb7f224cd807a34d1
Memphx <85446756+Memephix@users.noreply.github.com>
2024-04-02 00:05:55 +0700

    Adding Docker File for Dockerizing and deploying project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import Login from './Component/Login.js';
 import MainPage from './Component/MainPage';
-import History from './Component/History.js'
-import { BrowserRouter,Router,Route, Routes} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
   console.log("App is running")
 function App() {
